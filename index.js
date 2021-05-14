a47596a71978e480e53ecd8aee707fbb1a98a401
Hionu Anthony Chung <anthonychung97@gmail.com>
2021-05-13 23:11:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Anthony",
+    address: "College Park"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+
+    delete updatedEmployee[key];
+
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
